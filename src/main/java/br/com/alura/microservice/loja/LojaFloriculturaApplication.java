36246934af8ca49b63fd68ac65db04d9fdcbbbd6
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-21 22:30:25 -0300

    Implementando o eureka client e comunicação com o ms fornecedor

--- src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
+++ src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
@@ -2,10 +2,21 @@ package br.com.alura.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LojaFloriculturaApplication {
 
+	@Bean
+	@LoadBalanced // ESSE CARA VAI DAR A INTELIGENCIA AO REST TEMPLATE PARA RESOLVER A URL EM COMPRASERVICE
+	public RestTemplate getRestTemplte() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaFloriculturaApplication.class, args);
 	}
--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,29 +12,27 @@ import br.com.alura.microservice.loja.response.InfoFornecedorResponse;
 @Service
 public class CompraService {
 
-	
-	
-	
+	@Autowired
+	private RestTemplate client;
+
 	public void realizaCompra(CompraRequest request) {
 
-		// NOS PERMITE FAZER REQUISIÇÕES
-		RestTemplate client = new RestTemplate();
-		
 		// URL
-		String urlInfo = "http://localhost:8081/info/" + request.getEndereco().getEstado();
+		String urlInfo = "http://fornecedor/info/" + request.getEndereco().getEstado();
 
-		// DESSA FORMA VAMOS NOS COMUNICAR ATRAVES DE UM REQUEST HTTP SINCRONA 
-		// (SÓ PODEMOS MANDAR FAZER UMA REQUEST QUANDO NÃO TIVER NENHUMA OUTRA SENDO PROCESSADA)
-		ResponseEntity<InfoFornecedorResponse> exchange = client.exchange(urlInfo, 
+		// DESSA FORMA VAMOS NOS COMUNICAR ATRAVES DE UM REQUEST HTTP SINCRONA
+		// (SÓ PODEMOS MANDAR FAZER UMA REQUEST QUANDO NÃO TIVER NENHUMA OUTRA SENDO
+		// PROCESSADA)
+		ResponseEntity<InfoFornecedorResponse> exchange = client.exchange(urlInfo,
 				// MÉTODO
-				HttpMethod.GET, 
+				HttpMethod.GET,
 				// O QUE VAMOS ENVIAR
 				null,
 				// O QUE VAMOS RECEBER
 				InfoFornecedorResponse.class);
-		
+
 		System.out.println(exchange.getBody().getEndereco());
-		
+
 	}
 
 }
