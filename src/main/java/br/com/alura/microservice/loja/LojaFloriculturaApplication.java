6e0752c9940a5312b693e0af3dccfe3f98d8cb4d
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-27 21:23:22 -0300

    Implementando oauth

--- src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
+++ src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableResourceServer
 public class LojaFloriculturaApplication {
 
 	@Bean
