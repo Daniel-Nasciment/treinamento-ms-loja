940121c61e71282fa055fe4f721a3006089a5b65
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-28 10:10:17 -0300

    Implementando o repasse de headers para os clients

--- src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
+++ src/main/java/br/com/alura/microservice/loja/LojaFloriculturaApplication.java
@@ -7,8 +7,14 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.client.RestTemplate;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,6 +23,30 @@ import org.springframework.web.client.RestTemplate;
 @EnableResourceServer
 public class LojaFloriculturaApplication {
 
+	@Bean
+	public RequestInterceptor getInterceptorToken() {
+		return new RequestInterceptor() {
+			
+			// DA FORMA QUE ESTÁ O VALOR DE authentication ESTÁ VINDO NULL POR CONTA DO PROCESSAMENTO ESTAR OCORRENDO EM THREADS DIFERENTES
+			// POIS O PROCESSAMENTO DO MÉTODO REALIZA COMPRA ESTA ASSOCIADO AO POOL DE THREADS DO HYSTRIX
+			// É NECESSARIO CONFIGURAR ISSO NO APP PROPERTIES
+			
+			@Override
+			public void apply(RequestTemplate template) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if(authentication == null) {
+					return;
+				}
+				
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+				
+				String tokenValue = details.getTokenValue();
+			
+				template.header("Authorization", "Bearer" + tokenValue);
+			}
+		};
+	}
+	
 	@Bean
 	@LoadBalanced // ESSE CARA VAI DAR A INTELIGENCIA AO REST TEMPLATE PARA RESOLVER A URL EM COMPRASERVICE
 	// LOAD BALANCE DISTRIBUI A CARGA, OU SEJA, A CADA REUISICAO ELE VAI PROCURAR POR UM IP E PORTA DIFERENTE
--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -52,7 +54,6 @@ public class CompraService {
 		
 		// SPRING SLEUTH AJUDA  AGENTE A ACOMPANHAR LOGS, ELE CARREGA UM ID DA TRANSACAO POR TODOS OS DEMAIS MICRO SERVICOS
 		// ATE COMPLETAR A TRANSACAO
-		
 		
 		LOG.info("Realizando um pedido");
 		InfoPedidoResponse pedido = fornecedorClient.realizarPedido(request.getItens());
